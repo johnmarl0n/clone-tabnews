7954c489fafc955580c2073ba1bb5c342234b8c1
John Dias <napster472@gmail.com>
2024-11-21 00:59:50 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Priscila, eu te amo! Obrigado por ser essa pessoa maravilhosa que você é.</h1>
+    return <h1>Priscila, eu te amo! Obrigado por ser essa pessoa maravilhosa que você é!!!</h1>
 }
 
 export default Home;
