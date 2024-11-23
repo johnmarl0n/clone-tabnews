543463effe0cacd0794c95156e2489579721d091
John Dias <napster472@gmail.com>
2024-11-22 22:17:43 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do TabNews para o Curso.dev - Implementando do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Priscila, eu te amo! Obrigado por ser essa pessoa maravilhosa que você é!!!</h1>
+  return (
+    <h1>
+      Priscila, eu te amo! Obrigado por ser essa pessoa maravilhosa que você
+      é!!!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Teste de indentação");
 }
 
 export default Home;
